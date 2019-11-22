9bd0eff43977a732386212f457b6cfe185c3ad77
Jason J Cruz <jaycru06@yahoo.com>
2019-11-22 21:36:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
   ordered_dwarves = []
-  dwarves.each_with_index {|dwarf, number|}
+  dwarves.each_with_index {|dwarf, number| puts "#{number} #{dwarf}"}
 end
 
 def summon_captain_planet# code an argument here
