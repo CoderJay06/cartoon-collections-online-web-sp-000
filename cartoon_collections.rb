d31958c20443ff3cf6de33ab03fc9088313294ab
Jason J Cruz <jaycru06@yahoo.com>
2019-11-22 21:48:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,10 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_callse.each do |call| 
+    if call
+  end
 end
 
 def find_the_cheese# code an argument here
