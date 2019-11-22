f234791e946af32f9f6597c89bd438c29016f9c9
Jason J Cruz <jaycru06@yahoo.com>
2019-11-22 21:57:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.find? {|cheese| cheese == "cheddar" || cheese == 
+  ingredients.find? {|cheese| cheese == "cheddar" || cheese == "gouda" || cheese == 
 end
