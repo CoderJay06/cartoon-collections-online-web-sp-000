d4b59d0a62ac0d32b9a9516eebf903fddce8cc42
Jason J Cruz <jaycru06@yahoo.com>
2019-11-22 22:05:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ end
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
   ingredients.each do |cheese|
-    if cheese.include? "cheddar" || cheese.include? "gouda" || cheese.inclde? "camembert"
+    if cheese.include? "cheddar" || cheese.include? "gouda" || cheese.include? "camembert"
       return cheese
+    end
   end
 end
