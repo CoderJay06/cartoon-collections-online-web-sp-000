73baa4a8616a38073df7d71de0211917ab293d17
Jason J Cruz <jaycru06@yahoo.com>
2019-11-22 22:09:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(ingredients)
-  cheese_types = []
+  cheese_types = ["cheddar", "gouda", "camembert"]
   counter = 0 
   while counter < ingredients.length 
     counter +=1 
